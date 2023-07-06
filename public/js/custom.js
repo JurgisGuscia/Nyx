397db0fa0eb1c285398506fa847631134a7b99d8
JurgisGuscia <j.d.guscia@gmail.com>
2023-07-06 16:29:16 +0300

    adding copy code to clipboard buttons

--- public/js/custom.js
+++ public/js/custom.js
@@ -20,3 +20,17 @@ function check() {
     $("#add-new-item-form-container").toggleClass("hidden");
     
   });
+  var rows = $('.item-line-container');
+  for (var i = 0; i < rows.length; i++){
+    var button = rows[i].querySelector('button');
+    
+    button.addEventListener('click', function(e){
+      var link = e.target.closest('div').getElementsByTagName('div')[2].innerText;
+      var tempText = document.createElement('textarea');
+      tempText.value = link;
+      document.body.appendChild(tempText);
+      tempText.select();
+      document.execCommand('copy');
+      document.body.removeChild(tempText);
+    });
+  }
